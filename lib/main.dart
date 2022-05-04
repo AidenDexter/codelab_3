f53abe59c6d09c7adde9110e56085c41cb03492e
Dave <dave.bolotaev@gmail.com>
2022-05-04 20:55:33 +0300

    Убрал что-то

--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,11 @@ import 'package:flutter/material.dart';
 
 import 'package:codelab_3/pages/splash_page.dart';
 
-import 'pages/list_page.dart';
-
 void main() {
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
-      home:
-          // ListPage()
-          SplashPage(),
+      home: SplashPage(),
     ),
   );
 }
