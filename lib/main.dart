713d6052e9b6cb7e17696611edebaf6e9f5a4079
Dave <dave.bolotaev@gmail.com>
2022-05-05 18:18:52 +0300

    Landing Page Widget's Scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,12 @@ import 'package:flutter/material.dart';
 
 import 'package:codelab_3/pages/splash_page.dart';
 
+import './pages/landing_page.dart';
+
 void main() {
   runApp(
-    MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: SplashPage(),
-    ),
+    MaterialApp(debugShowCheckedModeBanner: false, home: LandingPage()
+        // SplashPage(),
+        ),
   );
 }
---
+++ lib/pages/landing_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class LandingPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: NetworkImage(
+                    'https://images.pexels.com/photos/261394/pexels-photo-261394.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'),
+                fit: BoxFit.cover,
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
